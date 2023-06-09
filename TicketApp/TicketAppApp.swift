e035b684aad8c7921824abf169773b2da66f190c
canknbr <cankanbur1@gmail.com>
2023-06-09 08:57:35 +0300

    Initial Commit

---
+++ TicketApp/TicketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketAppApp.swift
+//  TicketApp
+//
+//  Created by Can Kanbur on 9.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
