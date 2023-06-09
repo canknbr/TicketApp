1345970ff98e1db6706f068ff85ea2915a6841a5
canknbr <cankanbur1@gmail.com>
2023-06-09 10:28:17 +0300

    added ticket and tickets view

--- TicketApp/Views/TicketView.swift
+++ TicketApp/Views/TicketView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct TicketView: View {
+    @State var isAnimated = false
     var body: some View {
+        
         ZStack{
+            CircleBackground(color: Color("greenCircle"))
+                .blur(radius:isAnimated ? 30 :  100)
+                .offset(x:isAnimated ? -50 : -130,y:isAnimated ? -30 : -100)
+                .task {
+                    withAnimation(.easeInOut(duration: 7).repeatForever()){
+                        isAnimated.toggle()
+                    }
+                }
+            CircleBackground(color: Color("pinkCircle"))
+                .blur(radius:isAnimated ? 30 :  100)
+                .offset(x: isAnimated ? 100 : 130,y: isAnimated ? 150 : 100)
+              
             VStack(spacing: 30) {
-                Text("deneme")
+                Text("Mobile Ticket")
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .bold()
+                Text("Once you buy a movie ticket simply scan the barcode to access to your movie ")
+                    .frame(maxWidth: 248)
+                    .foregroundColor(.white)
+                    .font(.body)
+                    .multilineTextAlignment(.center)
             }
+            .padding(.horizontal,20)
             .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .top)
+            Tickets()
+                .padding(.top,30)
         }
         .background(LinearGradient(gradient: Gradient(colors: [Color("backgroundColor"),Color("backgroundColor2")]), startPoint: .top, endPoint: .bottom))
     }
